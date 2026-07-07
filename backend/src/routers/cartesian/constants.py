b9643ebf53898e9954c8c709cd3f5c4f6f7c9ed6
szeyoong-low <low.szeyoong@gmail.com>
2026-07-07 18:40:10 +0800

    feat: router path params updated according to ADR, query params introduced

--- backend/src/routers/cartesian/constants.py
+++ backend/src/routers/cartesian/constants.py
@@ -1,5 +1,6 @@
 # Path parameters
 METRIC_PATH_PARAM: str = "/{metric}"
+ANALYSIS_PATH_PARAM: str = "/{analysis}"
 
-# Paths (supported analysis)
+# Paths (supported display function)
 SIMPLE_TIME_SERIES_ENDPOINT: str = "/simple-time-series"
--- backend/src/routers/cartesian/router.py
+++ backend/src/routers/cartesian/router.py
@@ -1,10 +1,17 @@
-from fastapi import APIRouter
+from typing import Annotated
 
-from . import constants
+from fastapi import APIRouter, Query
+
+from . import constants as const
+from ...models import QueryParams
 
 router: APIRouter = APIRouter()
 
 
-@router.get(f"{constants.SIMPLE_TIME_SERIES_ENDPOINT}{constants.METRIC_PATH_PARAM}")
-def simple_time_series(metric: str):
-    return metric
+@router.get(
+    f"{const.SIMPLE_TIME_SERIES_ENDPOINT}{const.METRIC_PATH_PARAM}{const.ANALYSIS_PATH_PARAM}"
+)
+def simple_time_series(
+    metric: str, analysis: str, query: Annotated[QueryParams, Query()]
+):
+    return {"metric": metric, "analysis": analysis, **query.model_dump()}
