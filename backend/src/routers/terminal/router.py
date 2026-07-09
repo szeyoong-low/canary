8ce8db5d39881d3fbb8952bc2e0d7fa13d713bec
szeyoong-low <low.szeyoong@gmail.com>
2026-07-09 16:24:34 +0800

    feat: model validators for transformations

--- backend/src/routers/terminal/router.py
+++ backend/src/routers/terminal/router.py
@@ -1,7 +1,6 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Query
-from pydantic import BaseModel, ConfigDict
 
 
 TERMINAL_PREFIX = "/terminal"
@@ -13,20 +12,18 @@ DISPLAY_PATH_PARAM = "/{display}"
 TERMINAL_PATH = f"{METRIC_PATH_PARAM}{DISPLAY_PATH_PARAM}"
 
 
-class PathHandlerModel(BaseModel):
-    """Query parameters that must be present for all requests"""
-
-    # The raw query parameters will be passed to transformations to be parsed
-    # independently with their own Pydantic models.
-    model_config = ConfigDict(extra="ignore")
-
-    analysis: list[str]
-    view: list[str]
-    symbol: list[str]
-
-
 @router.get(TERMINAL_PATH)
 def terminal_path_op(
-    metric: str, display: str, query: Annotated[PathHandlerModel, Query()]
+    metric: str,
+    display: str,
+    analysis: Annotated[list[str], Query()],
+    view: Annotated[list[str], Query()],
+    symbol: Annotated[list[str], Query()],
 ):
-    return {"metric": metric, "display": display, **query.model_dump()}
+    return {
+        "metric": metric,
+        "display": display,
+        "analysis": analysis,
+        "view": view,
+        "symbol": symbol,
+    }
