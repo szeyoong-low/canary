3db9d3612d4796c833ff492d245a94abdeb28e78
szeyoong-low <low.szeyoong@gmail.com>
2026-07-09 12:45:30 +0800

    test: pure loading functionality of loader

---
+++ backend/test/loaders/constants.py
@@ -0,0 +1,5 @@
+from src.loaders import ExternalAPI
+
+LOADERS_CONSTANTS_PATH: str = "src.loaders.constants."
+NORMALISER_DISPATCH: str = "NORMALISER"
+FMP_API: ExternalAPI = "FMP"
---
+++ backend/test/loaders/test_load.py
@@ -0,0 +1,33 @@
+from json import load
+from typing import Any
+from unittest.mock import AsyncMock, Mock, patch
+
+from httpx import codes
+from polars import DataFrame, read_json
+from polars.testing import assert_frame_equal
+
+from . import constants as const
+from ..datasets import dataset_path
+from src.loaders import load_data
+
+
+# Internally-constructed dependencies are patched
+@patch.dict(
+    f"{const.LOADERS_CONSTANTS_PATH}{const.NORMALISER_DISPATCH}",
+    {const.FMP_API: lambda x: x},
+)
+async def test_load_only_regular():
+    """Loading a non-edge case without normalisation should return the original JSON"""
+    data_file: str = dataset_path("fmp_prod_segment_raw")
+    with open(data_file, mode="r") as data:
+        original_json: Any = load(data)
+
+    # Injected dependencies are mocked
+    response: Mock = Mock(status_code=codes.OK, **{"json.return_value": original_json})
+    http_client: Mock = Mock(get=AsyncMock(return_value=response))
+
+    actual: DataFrame = (
+        await load_data(http_client, const.FMP_API, "", {}, {})
+    ).collect()
+    expected: DataFrame = read_json(data_file)
+    assert_frame_equal(actual, expected)
