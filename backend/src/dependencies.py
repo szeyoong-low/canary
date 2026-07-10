45adad460b71ff480000992a419bedd2c9d4ce33
szeyoong-low <low.szeyoong@gmail.com>
2026-07-10 21:11:53 +0800

    feat: added FMP-related environment variables

--- backend/src/dependencies.py
+++ backend/src/dependencies.py
@@ -17,6 +17,8 @@ class Environment(BaseSettings):
 
     allow_origins: str
     allow_origin_regex: str
+    fmp_api_key: str
+    fmp_base_url: str
 
     model_config = SettingsConfigDict(env_file=DOTENV_FILE)
 
