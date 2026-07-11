311eae324fc96df33a0933ef8080f3bc2d57c8eb
szeyoong-low <low.szeyoong@gmail.com>
2026-07-11 11:00:11 +0800

    fix: differentiate between metric and metric group

--- backend/src/routers/terminal.py
+++ backend/src/routers/terminal.py
@@ -5,20 +5,20 @@ from fastapi import APIRouter, Query
 router = APIRouter(prefix="/terminal")
 
 # Path parameters
-METRIC_PATH_PARAM = "/{metric}"
+METRIC_GROUP_PATH_PARAM = "/{metric_group}"
 DISPLAY_PATH_PARAM = "/{display}"
-TERMINAL_PATH = f"{METRIC_PATH_PARAM}{DISPLAY_PATH_PARAM}"
+TERMINAL_PATH = f"{METRIC_GROUP_PATH_PARAM}{DISPLAY_PATH_PARAM}"
 
 
 @router.get(TERMINAL_PATH)
 def terminal_handler(
-    metric: str,
+    metric_group: str,
     display: str,
     analysis: Annotated[list[str], Query()],
     symbol: Annotated[list[str], Query()],
 ):
     return {
-        "metric": metric,
+        "metric_group": metric_group,
         "display": display,
         "analysis": analysis,
         "symbol": symbol,
