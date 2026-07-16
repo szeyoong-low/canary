c2e858b62a6e7189ce03daee2f245261f7aa9c55
szeyoong-low <low.szeyoong@gmail.com>
2026-07-16 16:36:45 +0800

    feat: loader for pending demo links

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,12 +1,26 @@
-import { Link } from "react-router";
-import { demoTitles } from "@/shared/constants";
+import { NavLink } from "react-router";
+import { BounceLoader } from "react-spinners";
+import { canaryThemeColour, demoTitles } from "@/shared/constants";
 
-function DemoLinkedList({demos}: {demos: string[]}) {
+function DemoLinkedList({ demos }: { demos: string[] }) {
   return (
     <ul>
       {demos.map((title, i) => (
-        <li key={i}>
-          <Link to={`/demo/${i}`}>{title}</Link>
+        <li key={i} className="flex items-center">
+          <NavLink to={`/demo/${String(i)}`}>
+            {({ isPending }) => (
+              <span>
+                {title}
+                {isPending && (
+                  <BounceLoader
+                    color={canaryThemeColour}
+                    size={"1em"}
+                    className="mx-2"
+                  />
+                )}
+              </span>
+            )}
+          </NavLink>
         </li>
       ))}
     </ul>
@@ -17,7 +31,7 @@ export default function Home() {
   return (
     <div>
       <p>Canary is here</p>
-      <DemoLinkedList demos={demoTitles}/>
+      <DemoLinkedList demos={demoTitles} />
     </div>
   );
 }
--- frontend/src/shared/constants.ts
+++ frontend/src/shared/constants.ts
@@ -1,5 +1,7 @@
 export const projectName = "Canary";
 
 export const demoTitles: string[] = [
-    "Share prices of major US tech firms and banks (start of 2026 is 100)",
-]
+  "Share prices of major US tech firms and banks (start of 2026 is 100)",
+];
+
+export const canaryThemeColour: string = "#ffc856";
