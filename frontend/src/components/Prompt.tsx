0625cc7b93a233a3be5bf7465ec42bc8362e1934
szeyoong-low <low.szeyoong@gmail.com>
2026-08-01 17:33:55 +0800

    feat: loading state for form submission

--- frontend/src/components/Prompt.tsx
+++ frontend/src/components/Prompt.tsx
@@ -1,18 +1,30 @@
 import { ArrowRight } from "lucide-react";
 import { useFetcher } from "react-router";
+import { BounceLoader } from "react-spinners";
+import { canaryThemeColour } from "@/shared/constants";
 
 export default function Prompt() {
   let fetcher = useFetcher();
   return (
-    <fetcher.Form method="POST" action="" className="PromptBox">
+    <fetcher.Form method="POST" className="PromptBox">
       <textarea
         className="PromptTextarea"
         name="prompt"
         placeholder="Ask a question here"
       />
-      <button className="PromptSubmit" type="submit">
-        <ArrowRight />
-      </button>
+      <div className="PromptSubmit">
+        {fetcher.state === "idle" ? (
+          <button className="cursor-pointer" type="submit">
+            <ArrowRight />
+          </button>
+        ) : (
+          <BounceLoader
+            color={canaryThemeColour}
+            size={"1em"}
+            className="cursor-progress mx-2"
+          />
+        )}
+      </div>
     </fetcher.Form>
   );
 }
