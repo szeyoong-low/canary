21d5955d1bb4406beae5361f5561a248eda530b3
szeyoong-low <low.szeyoong@gmail.com>
2026-08-17 21:12:54 +0800

    fix: comment out unused variables to satisfy tflint

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -1,5 +1,5 @@
 locals {
   env         = terraform.workspace # "production" or "development-pr-n"
-  is_prod     = local.env == "production"
-  name_suffix = local.env # for global/regional/account-level uniqueness
+  # is_prod     = local.env == "production"
+  # name_suffix = local.env # for global/regional/account-level uniqueness
 }
