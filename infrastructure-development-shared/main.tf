d8beb85c1c20a305e4a1f60bf9273fca596e890d
szeyoong-low <low.szeyoong@gmail.com>
2026-08-23 23:06:35 +0800

    feat: shared development infrastructure

---
+++ infrastructure-development-shared/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "eu-west-2" // London
+
+  default_tags {
+    tags = {
+      managed_by  = "terraform"
+      environment = local.environment // `environment` is load-bearing: used for ABAC.
+    }
+  }
+}
+
+locals {
+  environment = "development-shared"
+}
