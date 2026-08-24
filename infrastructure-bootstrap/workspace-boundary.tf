96e7a2f4fe19a09f8f7f3b858bd5762cca43e96b
szeyoong-low <low.szeyoong@gmail.com>
2026-08-24 14:39:30 +0800

    debug: identified problem at DenyUntaggedResourceCreation

--- infrastructure-bootstrap/workspace-boundary.tf
+++ infrastructure-bootstrap/workspace-boundary.tf
@@ -80,18 +80,24 @@ resource "aws_iam_policy" "workspace_boundary" {
         Resource = "*"
       },
       {
-        Sid    = "DenyUntaggedResourceCreation"
-        Effect = "Deny"
-        Action = [
-          "iam:CreateRole",
-          "iam:CreateUser",
-          "ec2:CreateVpc",
-          "ec2:CreateSubnet",
-        ]
+        Sid      = "DenyCrossEnvironmentResourceAccess"
+        Effect   = "Deny"
+        Action   = "*"
         Resource = "*"
         Condition = {
-          Null = {
-            "aws:RequestTag/environment" = "true"
+          StringNotEquals = {
+            "aws:ResourceTag/environment" = "$${aws:PrincipalTag/environment}"
+          }
+        }
+      },
+      {
+        Sid      = "DenyForeignEnvironmentTagOnCreation"
+        Effect   = "Deny"
+        Action   = "*"
+        Resource = "*"
+        Condition = {
+          StringNotEquals = {
+            "aws:RequestTag/environment" = "$${aws:PrincipalTag/environment}"
           }
         }
       },
