872b095cc6d5f3387ae1099ab4680c73f7df1127
szeyoong-low <low.szeyoong@gmail.com>
2026-08-24 14:26:38 +0800

    debug: remove DenyCrossEnvironmentResourceAccess and DenyForeignEnvironmentTagOnCreation to isolate failure

--- infrastructure-bootstrap/workspace-boundary.tf
+++ infrastructure-bootstrap/workspace-boundary.tf
@@ -80,28 +80,6 @@ resource "aws_iam_policy" "workspace_boundary" {
         Resource = "*"
       },
       {
-        Sid      = "DenyCrossEnvironmentResourceAccess"
-        Effect   = "Deny"
-        Action   = "*"
-        Resource = "*"
-        Condition = {
-          StringNotEquals = {
-            "aws:ResourceTag/environment" = "$${aws:PrincipalTag/environment}"
-          }
-        }
-      },
-      {
-        Sid      = "DenyForeignEnvironmentTagOnCreation"
-        Effect   = "Deny"
-        Action   = "*"
-        Resource = "*"
-        Condition = {
-          StringNotEquals = {
-            "aws:RequestTag/environment" = "$${aws:PrincipalTag/environment}"
-          }
-        }
-      },
-      {
         Sid    = "DenyUntaggedResourceCreation"
         Effect = "Deny"
         Action = [
