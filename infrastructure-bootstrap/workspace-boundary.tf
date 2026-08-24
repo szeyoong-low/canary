f28df75ea977518fbbcf6a274718bfbd4faad928
szeyoong-low <low.szeyoong@gmail.com>
2026-08-24 14:39:57 +0800

    debug: identified problem at DenyCrossEnvironmentResourceAccess

--- infrastructure-bootstrap/workspace-boundary.tf
+++ infrastructure-bootstrap/workspace-boundary.tf
@@ -80,17 +80,6 @@ resource "aws_iam_policy" "workspace_boundary" {
         Resource = "*"
       },
       {
-        Sid      = "DenyCrossEnvironmentResourceAccess"
-        Effect   = "Deny"
-        Action   = "*"
-        Resource = "*"
-        Condition = {
-          StringNotEquals = {
-            "aws:ResourceTag/environment" = "$${aws:PrincipalTag/environment}"
-          }
-        }
-      },
-      {
         Sid      = "DenyForeignEnvironmentTagOnCreation"
         Effect   = "Deny"
         Action   = "*"
