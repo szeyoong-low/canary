0b767b97d4a82db87025f291b5e0f7962427a2d8
szeyoong-low <low.szeyoong@gmail.com>
2026-08-28 23:09:23 +0800

    devops: add AWS provider to global infrastructure

--- infrastructure-global/main.tf
+++ infrastructure-global/main.tf
@@ -1,4 +1,17 @@
+provider "aws" {
+  region = "eu-west-2" # London
+
+  default_tags {
+    tags = {
+      managed_via = "terraform"
+      environment = local.environment // `environment` is load-bearing: used for ABAC
+    }
+  }
+}
+
 locals {
+  environment = "global"
+
   # Owns the canary.markets domain
   cloudflare_zone_id = "298ffd9b0153fe3a36795cda6401f8e9"
 
