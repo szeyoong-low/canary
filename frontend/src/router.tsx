a7d88d1b64b7856fc6e58539487485b32e3bb38b
szeyoong-low <low.szeyoong@gmail.com>
2026-09-06 22:36:42 +0800

    feat: clean up home page

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,19 +1,10 @@
 import { createBrowserRouter } from "react-router";
-import { getChartFromPrompt, loadChartConfig } from "@/lib/api";
-import { Ask, Demo, Error, Home, Layout } from "@/routes";
+import { Error, Home, Layout } from "@/routes";
 
 export default createBrowserRouter([
   {
     Component: Layout,
     ErrorBoundary: Error,
-    children: [
-      { index: true, Component: Home },
-      { path: "demo/ask", Component: Ask, action: getChartFromPrompt },
-      {
-        path: "demo/:demoID",
-        Component: Demo,
-        loader: loadChartConfig,
-      },
-    ],
+    children: [{ index: true, Component: Home }],
   },
 ]);
--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,32 +1,3 @@
-import { Link, NavLink } from "react-router";
-import { BounceLoader } from "react-spinners";
-import { canaryThemeColour, demoTitles } from "@/shared/constants";
-
-function DemoLinkedList({ demos }: { demos: string[] }) {
-  return (
-    <ul className="list-disc">
-      {demos.map((title: string, i: number) => (
-        <li key={i}>
-          <NavLink to={`/demo/${String(i)}`}>
-            {({ isPending }: { isPending: boolean }) => (
-              <span className="flex flex-row items-center">
-                {title}
-                {isPending && (
-                  <BounceLoader
-                    color={canaryThemeColour}
-                    size={"1em"}
-                    className="mx-2"
-                  />
-                )}
-              </span>
-            )}
-          </NavLink>
-        </li>
-      ))}
-    </ul>
-  );
-}
-
 export default function Home() {
   return (
     <div className="flex justify-center">
@@ -37,18 +8,6 @@ export default function Home() {
           questions about market movements and trade flows into professional
           reports with beautiful charts and insightful analysis.
         </p>
-        <p>
-          Ask away{" "}
-          <Link to="/demo/ask" className="text-theme font-medium">
-            here
-          </Link>
-          !
-        </p>
-        <p>
-          It is able to fetch third-party data and transform it into a variety
-          of charts, including time series and treemaps.
-        </p>
-        <DemoLinkedList demos={demoTitles} />
         <p>
           For the latest progress and upcoming features, see the{" "}
           <a
