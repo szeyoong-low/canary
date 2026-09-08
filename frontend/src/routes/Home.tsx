c263e9d8b9210280b2d250ffefe9d0b0513009e1
szeyoong-low <low.szeyoong@gmail.com>
2026-09-08 23:09:08 +0800

    feat: outline of home page

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -4,16 +4,44 @@ export default function Home() {
   return (
     <div className="flex justify-center">
       <div className="mx-10 md:mx-0 md:w-175 flex flex-col gap-4 text-justify">
-        <p>
-          <span className="emphasis">Canary's</span> vision is to be an agentic
-          Jupyter notebook for financial analysts. It turns your questions about
-          market movements and trade flows into professional reports with{" "}
-          <Link to="/report/1" className="emphasis">
-            beautiful charts
-          </Link>{" "}
-          and insightful analysis.
-        </p>
+        <Hero />
+        <CallToAction />
       </div>
+    </div>
+  );
+}
+
+function Hero() {
+  return (
+    <div className="font-(family-name:--title-font-family) font-medium">
+      <h2>
+        <span className="text-theme">Canary</span>
+        <img src="favicon.svg" />
+        <br />
+        crystallises <br />
+        chaos <br />
+        into <br />
+        charts
+      </h2>
+    </div>
+  );
+}
+
+function CallToAction() {
+  return (
+    <div>
+      <p>
+        <span className="emphasis">Canary</span> is an agentic Jupyter Notebook
+        for financial analysts.
+      </p>
+      <p>
+        It turns your questions about market movements and trade flows into
+        professional reports with compelling, elegant charts.
+        <br />
+      </p>
+      <Link to="/report/1" className="emphasis">
+        Create yours today
+      </Link>
     </div>
   );
 }
